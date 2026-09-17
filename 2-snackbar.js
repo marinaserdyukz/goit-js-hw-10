33f2d200952d0c7cbfb08b4ac93e6630adf03ec7
marinaserdyukz <153117820+marinaserdyukz@users.noreply.github.com>
2026-09-17 07:18:59 +0000

    Deploying to gh-pages from @ marinaserdyukz/goit-js-hw-10@c30a69fd4221a97ee2d50ad8c5c6b63cf6b0d8c6 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as m}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",o=>{o.preventDefault();const t=Number(s.elements.delay.value),r=s.elements.state.value;new Promise((e,i)=>{setTimeout(()=>{r==="fulfilled"?e(t):i(t)},t)}).then(e=>{m.success({message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{m.error({message:`❌ Rejected promise in ${e}ms`})}),s.reset()});
+//# sourceMappingURL=2-snackbar.js.map
